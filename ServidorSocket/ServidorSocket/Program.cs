e2f2e5625aa735faa8165ea1363112cb8da16c29
MharkozBezerra <51019697+MharkozBezerra@users.noreply.github.com>
2021-09-18 14:31:03 -0300

    servidor criado

--- ServidorSocket/ServidorSocket/Program.cs
+++ ServidorSocket/ServidorSocket/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ServidorSocket
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
+
+
+
         }
     }
 }
