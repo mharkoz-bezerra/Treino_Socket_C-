7096504204afca3bb040f82ed6d5278d0e894d55
MharkozBezerra <51019697+MharkozBezerra@users.noreply.github.com>
2021-09-18 15:47:40 -0300

    cliente finalizado, e pronto para o marge

---
+++ ServidorSocket/ServidorSocket/Geral/Cliente.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace ServidorSocket.Geral
+{
+    class Cliente
+    {
+       private static Socket Ouvinte{get;set;}
+       private static IPEndPoint Conectar { get; set; }
+
+        public static void EnviarMensagem() {
+
+            //Verifica se servidor encontra-se online
+            if (!ConectarServidor()) {
+                Console.WriteLine("Servidor não está Online!!!");
+                return;
+            }
+            
+                byte[] bytes = new byte[1024];              //Define o tamanho máximo do byte
+                Console.WriteLine("Digite sua mensagem: "); //Solicita ao usuário que digite sua mensagem
+                string dados = Console.ReadLine();          //Coleta a informação diggitada pelo usuário
+                bytes = Encoding.Default.GetBytes(dados);   //Converte a informação para um array de byte
+                Ouvinte.Send(bytes);                        //Envia mensagem para o servidor
+                Console.ReadKey();                          //Utilizado para não finalizar o programa
+                
+            
+            
+        }
+        public static bool ConectarServidor() {
+            try
+            {
+                Ouvinte = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Conectar = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+                Ouvinte.Connect(Conectar);
+                return Ouvinte.Connected;
+            }
+            catch (Exception e) {
+                Console.WriteLine($"Erro encontrado: {e.Message}");
+                return false;
+            }
+            
+        }
+    }
+}
--- ServidorSocket/ServidorSocket/Program.cs
+++ ServidorSocket/ServidorSocket/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServidorSocket.Geral;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,45 @@ namespace ServidorSocket
 {
     class Program
     {
+        public static IFormatProvider numServidor { get; private set; }
+
         static void Main(string[] args)
         {
+            bool _rodando = true;
+            //Coleta a informação para o usuário;
+            Console.WriteLine("Sistema de Troca de Mensagens entre cliente-servidor \n");
+            
+
+            while (_rodando) {
+                int numServidor = 9999;
+                Console.WriteLine("Digite as informações presentes para iniciar \n");
+                Console.WriteLine("0 - Servidor | 1 - Cliente  |5 - Sair");
+                string dados = Console.ReadLine();
+                if (!Int32.TryParse(dados, out numServidor)) {
+                    Console.WriteLine("Informação passada é incorreta \n");
+                    return;
+                }
+
+                if (numServidor != 0 ^ numServidor != 1 ^ numServidor != 5) {
+                    Console.WriteLine("Informação passada é incorreta \n");
+                    return;
+                }
+                switch (numServidor) {
+
+                    case 0:
+                        Console.WriteLine("Modo Servidor Ativo");
+                        break;
+                    case 1:
+                        Console.WriteLine("Modo Cliente Ativo");
+                        Cliente.EnviarMensagem();
+                        break;
+                    case 5:
+                        Console.WriteLine("\n Processo finalizado!!!");
+                        _rodando = false;
+                        break;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
