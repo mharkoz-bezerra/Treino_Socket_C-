0dcb06ecc985767669ee9c35001072543ad555ce
MharkozBezerra <51019697+MharkozBezerra@users.noreply.github.com>
2021-09-19 22:11:50 -0300

    Servidor Criado
    Proximo passo será criado o cliente

--- Treino_Socket_III/Treino_Socket_III/Geral/Usuario.cs
+++ Treino_Socket_III/Treino_Socket_III/Geral/Usuario.cs
@@ -11,11 +11,16 @@ namespace Treino_Socket_III.Geral
     {
         public string Login { get; set; }
         public string Senha { get; set; }
+        public string ID { get; set; }
+        public string Nome { get; set; }
 
-        public Usuario(string lg, string pwd) {
+        public Usuario(string lg = "", string pwd = "", string id = "", string nome = "") {
             this.Login = lg;
             this.Senha = pwd;
+            this.Nome = nome;
+            this.ID = id;
         }
+    
 
     }
 }
