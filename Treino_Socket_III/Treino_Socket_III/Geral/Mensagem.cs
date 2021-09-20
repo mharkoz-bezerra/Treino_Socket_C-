a2be4a3a8185d8d8a172a4b433a205420f507630
MharkozBezerra <51019697+MharkozBezerra@users.noreply.github.com>
2021-09-19 21:07:56 -0300

    Montando Classe mensagem

---
+++ Treino_Socket_III/Treino_Socket_III/Geral/Mensagem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treino_Socket_III.Geral
+{
+    [Serializable]
+    class Mensagem
+    {
+        public Usuario DeUsuario { get; set; }
+        public Usuario ParaUsuario { get; set; }
+        public string Msg { get; set; }
+
+        public Mensagem(Usuario de, Usuario para, string mensagem) {
+            this.DeUsuario =de;
+            this.ParaUsuario = para;
+            this.Msg = mensagem; 
+        }
+
+    }
+}
